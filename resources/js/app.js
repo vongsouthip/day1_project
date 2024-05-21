bd2ec9e9bd23384e155075bd261d9bde66ebd891
vongsouthip <69452350+vongsouthip@users.noreply.github.com>
2024-05-21 11:49:30 +0700

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+
+import { createApp } from 'vue';
+import App from './app.vue';
+
+const app = createApp(App);
+app.mount('#app-vue');
---
+++ resources/js/app.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <h1>hello vue App</h1>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+
+</style>
